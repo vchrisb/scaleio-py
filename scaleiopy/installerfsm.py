3143f4274c5119a7c34f3b950d9ef4f7c254892c
vchrisb <vchrisb@users.noreply.github.com>
2015-08-14 09:54:55 +0200

    increased sleep

--- scaleiopy/installerfsm.py
+++ scaleiopy/installerfsm.py
@@ -21,7 +21,7 @@ class Transition(object):
 	# Code executed when transitioning from one state to another
 	def __init__(self, toState):
 		self.toState = toState
-		
+
 	def Execute(self):
 		print ("Transitioning...")
 		print ""
@@ -39,20 +39,20 @@ class State(object):
 		im_instance = im_inst
 		# Possible states: IDLE, PENDING, FAILED, COMPLETED
 		#print self.FSM.getCurrentStateStatus()
-		
+
 	def Enter(self):
 		self.FSM.setCurrentStateStatus('PENDING')
 		pass
-	
+
 	def Execute (self):
 		pass
-	
+
 	def status (self):
 		pass
-	
+
 	def Next(self):
 		pass
-	
+
 	def Exit(self):
 		pass
 
@@ -61,7 +61,7 @@ class Query(State):
 	def __init__(self, FSM, im_inst):
 		super(Query, self).__init__(FSM, im_inst)
 		self.calling_object = im_inst
-		
+
 	def Enter(self):
 		print ("Entering QUERY phase")
 		super(Query, self).Enter()
@@ -73,7 +73,7 @@ class Query(State):
 		self.pendingItemCount = self.FSM.install_process_status("query")
 		while self.pendingItemCount > 0:
 			self.pendingItemCount = self.FSM.install_process_status("query")
-			time.sleep(0.5)
+			time.sleep(2)
 		if self.pendingItemCount < 0:
 			print "*** FAILED ***"
 		else:
@@ -86,7 +86,7 @@ class Query(State):
 	def Next(self):
 		#print ("Advance to next step")
 		self.FSM.ToTransition("toUPLOAD") # Move to next step
-		
+
 	def Exit(self):
 		pass
 		#print ("Exiting QUERY phase.")
@@ -95,7 +95,7 @@ class Upload(State):
 	# Worker class - Takes care of managing IM QUERY phase and do status control of each phase
 	def __init__(self, FSM, im_inst):
 		super(Upload, self).__init__(FSM, im_inst)
-		
+
 	def Enter(self):
 		print ("Entering UPLOAD phase")
 		super(Upload, self).Enter()
@@ -114,14 +114,14 @@ class Upload(State):
 		else:
 			print "*** COMPLETED ***"
 			self.Next()
-		
+
 		if self.FSM.autoTransition:
 			self.FSM.Execute()
-	
+
 	def Next(self):
 		#print ("Advance to next step")
 		self.FSM.ToTransition("toINSTALL") # Move to next step
-		
+
 	def Exit(self):
 		pass
 		#print ("Exiting UPLOAD phase.")
@@ -131,7 +131,7 @@ class Install(State):
 	# Worker class - Takes care of managing IM Install phase and do status control of each phase
 	def __init__(self, FSM, im_inst):
 		super(Install, self).__init__(FSM, im_inst)
-		
+
 	def Enter(self):
 		print ("Entering install phase")
 		super(Install, self).Enter()
@@ -150,14 +150,14 @@ class Install(State):
 			print "*** COMPLETED ***"
 			time.sleep(6)
 			self.Next()
-		
+
 		if self.FSM.autoTransition:
 			self.FSM.Execute()
-			
+
 	def Next(self):
 		print ("Advance to next step")
 		self.FSM.ToTransition("toCONFIGURE") # Move to next step
-		
+
 	def Exit(self):
 		print ("Exiting INSTALL phase.")
 
@@ -166,7 +166,7 @@ class Configure(State):
 	# Worker class - Takes care of managing IM Configure phase and do status control of each phase
 	def __init__(self, FSM, im_inst):
 		super(Configure, self).__init__(FSM, im_inst)
-		
+
 	def Enter(self):
 		print ("Entering configure phase")
 		super(Configure, self).Enter()
@@ -188,11 +188,11 @@ class Configure(State):
 
 		if self.FSM.autoTransition:
 			self.FSM.Execute()
-		
+
 	def Next(self):
 		#print ("Advance to next step")
 		self.FSM.ToTransition("toARCHIVE") # Move to next step
-		
+
 	def Exit(self):
 		pass
 		#print ("Exiting CONFIGURE phase.")
@@ -201,7 +201,7 @@ class Archive(State):
 	# Worker class - Takes care of managing IM Archive phase and do status control of each phase
 	def __init__(self, FSM, im_inst):
 		super(Archive, self).__init__(FSM, im_inst)
-		
+
 	def Enter(self):
 		print ("Entering archive phase")
 		super(Archive, self).Enter()
@@ -212,15 +212,15 @@ class Archive(State):
 		self.FSM.imapi.set_archive_all()
 		print self.FSM.imapi.get_state()
 		time.sleep(3)
-		
+
 		self.Next()
 		if self.FSM.autoTransition:
 			self.FSM.Execute()
-		
+
 	def Next(self):
 		print ("Advance to next step")
 		self.FSM.ToTransition("toCOMPLETE") # Move to next step
-		
+
 	def Exit(self):
 		print ("Exiting ARCHIVE phase.")
 
@@ -228,7 +228,7 @@ class Complete(State):
 	# Worker class - Takes care of managing IM "Completing" phase and do status control of each phase
 	def __init__(self, FSM, im_inst):
 		super(Complete, self).__init__(FSM, im_inst)
-		
+
 	def Enter(self):
 		print ("Entering COMPLETE phase")
 		super(Complete, self).Enter()
@@ -238,7 +238,7 @@ class Complete(State):
 		self.FSM.setCurrentStateStatus('COMPLETED')
 		print self.FSM.getCurrentStateStatus()
 		print self.FSM.imapi.get_state()
-		
+
 	def Next(self):
 		self.FSM.setCurrentStateStatus('COMPLETED')
 
@@ -249,7 +249,7 @@ class Complete(State):
 ## FINITE STATE MACHINE
 
 class FSM(object):
-	# Holds the states and transitions available, 
+	# Holds the states and transitions available,
 	# executes current states main functions and transitions
 	def __init__(self, imapi):
 		self.states = {}
@@ -264,10 +264,10 @@ class FSM(object):
 
 	def AddTransition(self, transName, transition):
 		self.transitions[transName] = transition
-		
+
 	def enableAutoTransition(self):
 		self.autoTransition = True
-	
+
 	def disableAutoTransition(self):
 		self.autoTransition = False
 
@@ -277,22 +277,22 @@ class FSM(object):
 	def SetState(self, stateName):
 		self.prevState = self.curState
 		self.curState = self.states[stateName]
-	
+
 	def getState(self):
 		return curState
-	
+
 	def setCurrentStateStatus(self, status):
-		print "*** Setting currentStateStatus to " + status + " ***" 
+		print "*** Setting currentStateStatus to " + status + " ***"
 		self.curStateStatus = status
 		if status == 'COMPLETED':
 			self.trans = None
 		#	self.curState = None
 		#print "setCurrentStateStatus() = " + self.curStateStatus
-	
+
 	def getCurrentStateStatus(self):
 		return self.curStateStatus
 		# Return IDLE, FAILED, PENDING, COMPLETE
-		
+
 	def ToTransition(self, toTrans):
 		self.trans = self.transitions[toTrans]
 
@@ -304,7 +304,7 @@ class FSM(object):
 		else:
 			self.trans = None
 			print " Status is COMPLETE - Next() will not do anything"
-			
+
 	def Execute(self):
 		print "FSM Execute() - curStateStatus = " + self.curStateStatus
 		if self.curStateStatus == 'IDLE' or self.curStateStatus == 'PENDING':
@@ -320,7 +320,7 @@ class FSM(object):
 			print "Statemachine COMPLETE"
 			self.trans = None
 
-	
+
 	def install_process_status(self, expectState):
 		self.imapi.get_state()
 		self.cStatusItems = json.loads(self.imapi.get_command_state())
@@ -339,7 +339,7 @@ class FSM(object):
 		else:
 			return self.pendingItemCount
 
-		# IM State:        
+		# IM State:
 		# idle, query, upload, install, configure
 		# IM Command state:
 		# pending, failed, completed
@@ -356,7 +356,7 @@ class InstallerFSM:
 		self.FSM = FSM(imapi)
 		if automatic:
 			self.FSM.enableAutoTransition()
-			
+
 		## STATES
 		self.FSM.AddState("QUERY", Query(self.FSM, imapi))
 		self.FSM.AddState("UPLOAD", Upload(self.FSM, imapi))
@@ -372,15 +372,14 @@ class InstallerFSM:
 		self.FSM.AddTransition("toCONFIGURE", Transition("CONFIGURE"))
 		self.FSM.AddTransition("toARCHIVE", Transition("ARCHIVE"))
 		self.FSM.AddTransition("toCOMPLETE", Transition("COMPLETE"))
-		
+
 		self.FSM.SetState("QUERY") # When executing FSM first time always start at QUERY phase
-	
+
 	def Next(self):
 		self.FSM.Next()
-	
+
 	def getCurrentStateStatus(self):
 		return self.FSM.getCurrentStateStatus()
-		
+
 	def Execute(self):
 		self.FSM.Execute()
-
